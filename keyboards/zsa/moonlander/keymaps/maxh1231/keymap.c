d870146909a3eb3b3fcd832dfb1ac181d8299bfd
max <max@humpherys.dev>
2025-06-20 23:33:17 -0600

    win and nav layers

--- keyboards/zsa/moonlander/keymaps/maxh1231/keymap.c
+++ keyboards/zsa/moonlander/keymaps/maxh1231/keymap.c
@@ -1,6 +1,7 @@
 #include QMK_KEYBOARD_H
 #include "version.h"
-
+#define MOON_LED_LEVEL LED_LEVEL
+#define ML_SAFE_RANGE SAFE_RANGE
 enum layers {
     MAC,
     WIN,
@@ -13,20 +14,28 @@ enum custom_keycodes {
     VRSN = SAFE_RANGE,
 };
 
+// home row mods
 #define CTL_ESC MT(MOD_LCTL, KC_ESC)
 #define ALT_A MT(MOD_LALT, KC_A)
 #define SFT_D MT(MOD_LSFT, KC_D)
-#define LNUM_F LT(NUM, KC_F)
-#define LNAV_J LT(NAV, KC_J)
 #define SFT_K MT(MOD_RSFT, KC_K)
 #define ALT_SCN MT(MOD_RALT, KC_SCLN)
+// layer mods
+#define LNUM_F LT(NUM, KC_F)
+#define LNAV_J LT(NAV, KC_J)
 #define LSYM_BS LT(SYMB, KC_BSPC)
+// tmux leader
 #define TMUX_L LCTL(KC_A)
+// media keys
+#define MEDPT KC_MEDIA_PREV_TRACK
+#define MEDNT KC_MEDIA_NEXT_TRACK
+#define MEDPP KC_MEDIA_PLAY_PAUSE
+//
 
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [MAC] = LAYOUT(
-        XXXXXXX,   KC_1,   KC_2,   KC_3,   KC_4,   KC_5,XXXXXXX,       XXXXXXX,   KC_6,   KC_7,   KC_8,   KC_9,   KC_0,XXXXXXX,
+        XXXXXXX,   KC_1,   KC_2,   KC_3,   KC_4,   KC_5,XXXXXXX,       XXXXXXX,   KC_6,   KC_7,   KC_8,   KC_9,   KC_0,TD(DANCE_0),
         KC_TAB,    KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,XXXXXXX,       XXXXXXX,   KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,KC_BSLS,
         CTL_ESC,  ALT_A,   KC_S,  SFT_D, LNUM_F,   KC_G,XXXXXXX,       XXXXXXX,   KC_H, LNAV_J,  SFT_K,   KC_L,ALT_SCN,KC_QUOT,
         KC_LSFT,   KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,                          KC_N,   KC_M,KC_COMM, KC_DOT,KC_SLSH,XXXXXXX,
@@ -34,6 +43,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                         LSYM_BS, KC_DEL,XXXXXXX,        TMUX_L, KC_ENT, KC_SPC
     ),
 
+    [WIN] = LAYOUT(
+        _______,_______,_______,_______,_______,_______,_______,           _______,_______,_______,_______,_______,_______,TD(DANCE_1),
+        _______,_______,_______,_______,_______,_______,_______,           _______,_______,_______,_______,_______,_______,_______,
+        _______,_______,_______,_______,_______,_______,_______,           _______,_______,_______,_______,_______,_______,_______,
+        _______,_______,_______,_______,_______,_______,                           _______,_______,_______,_______,_______,_______,
+        _______,KC_LGUI,_______,_______,KC_LALT,_______,                   _______,_______,_______,_______,_______,_______,
+                                        _______,_______,_______,        ST_MACRO_0,_______,_______
+    ),
     [SYMB] = LAYOUT(
         _______,_______,_______,_______,_______,_______,_______,       _______,_______,_______,_______,_______,_______,XXXXXXX,
         _______, KC_GRV,KC_PIPE,KC_LABK,KC_RABK,KC_UNDS,_______,       _______,KC_DQUO,KC_LCBR,KC_RCBR, KC_DLR,KC_PERC,KC_BSPC,
@@ -44,10 +61,18 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
 
     [NUM] = LAYOUT(
-        _______,_______,_______,_______,_______,_______,_______,      _______,_______,_______,_______,_______,_______,_______,
-        _______,_______,_______,_______,_______,_______,_______,      _______,_______,   KC_7,   KC_8,   KC_9,_______,_______,
-        _______,_______,_______,_______,_______,_______,_______,      _______,_______,   KC_4,   KC_5,   KC_6,_______,_______,
-        _______,_______,_______,_______,_______,_______,                      _______,   KC_1,   KC_2,   KC_3,_______,_______,
+        _______,_______,_______,_______,_______,_______,_______,      _______,_______, KC_TAB,KC_PSLS,KC_PAST,KC_MINS,_______,
+        _______,_______,_______,_______,_______,_______,_______,      _______,_______,   KC_7,   KC_8,   KC_9,KC_PLUS,_______,
+        _______,_______,_______,_______,_______,_______,_______,      _______,_______,   KC_4,   KC_5,   KC_6, KC_ENT,_______,
+        _______,_______,_______,_______,_______,_______,                      _______,   KC_1,   KC_2,   KC_3, KC_ENT,_______,
+        _______,_______,_______,_______,_______,_______,                      _______,   KC_0,_______, KC_DOT,_______,_______,
+                                        _______,_______,_______,      _______,_______,_______
+    ),
+    [NAV] = LAYOUT(
+        _______,_______,  MEDPT,  MEDPP,  MEDNT,_______,_______,      _______,_______,_______,_______,_______,_______,_______,
+        _______,_______,_______,  KC_UP,_______,_______,_______,      _______,_______,_______,_______,_______,_______,_______,
+        _______,_______,KC_LEFT,KC_DOWN,KC_RGHT,_______,_______,      _______,_______,_______,KC_RSFT,_______,KC_RALT,KC_RCTL,
+        _______,_______,_______,_______,_______,_______,                      _______,_______,_______,_______,_______,_______,
         _______,_______,_______,_______,_______,_______,                      _______,_______,_______,_______,_______,_______,
                                         _______,_______,_______,      _______,_______,_______
     ),
