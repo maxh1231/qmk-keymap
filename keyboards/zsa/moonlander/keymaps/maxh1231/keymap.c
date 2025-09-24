73ab7d5358b603e26be5700cdd63fe3a9926f704
max <max@humpherys.dev>
2025-09-23 19:57:00 -0600

    gaming layer

--- keyboards/zsa/moonlander/keymaps/maxh1231/keymap.c
+++ keyboards/zsa/moonlander/keymaps/maxh1231/keymap.c
@@ -46,7 +46,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,    KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,XXXXXXX,       XXXXXXX,   KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,KC_BSLS,
         CTL_ESC,   KC_A,   KC_S,   KC_D,   KC_F,   KC_G,XXXXXXX,       XXXXXXX,   KC_H,   KC_J,   KC_K,   KC_L,KC_SCLN,KC_QUOT,
         KC_LSFT,   KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,                          KC_N,   KC_M,KC_COMM, KC_DOT,KC_SLSH,KC_RSFT,
-        KC_LCTL,XXXXXXX,XXXXXXX,XXXXXXX,KC_LGUI,XXXXXXX,                       XXXXXXX,KC_RALT,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,
+        KC_LALT,XXXXXXX,XXXXXXX,XXXXXXX,KC_LGUI,XXXXXXX,                       XXXXXXX,KC_RALT,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,
                                         KC_BSPC, KC_DEL,LNUM_NO,       LNAV_TM, KC_ENT, KC_SPC
     ),
 
